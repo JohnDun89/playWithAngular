5b45bc9a6892c997cb0f02fcc04db6dacbd76597
John Duncan <john@johnduncanphoto.com>
2018-01-29 19:55:39 +0000

    more functionality added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, style, transition, animate, keyframes, query, stagger} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl:'./home.component.html',
-  styles: [`
-  p {font-weight: bold }
-  div [color: grey; ]
-  `]
+  styleUrls: ['./home.component.css'],
+  animations:
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number;
+  btnText: string = 'Add an Item';
+  goalText: string = 'my first life goal';
+  goals = [];
   constructor() { }
 
+  //all logic goes underneath the constructor
+
+// ng on init runs any code when the component is loaded
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem(){
+    //logic to handle a null input
+    if (this.goalText === "") {
+      return;
+    }
+    this.goals.push(this.goalText);
+    // this resets the text once the button has been pressed
+    this.goalText = '';
+    this.itemCount = this.goals.length;
   }
 
 }
