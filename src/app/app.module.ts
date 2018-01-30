9d10360980cf7ccb9dab3f965b3d5a88ea7c6a8f
John Duncan <john@johnduncanphoto.com>
2018-01-30 10:48:58 +0000

    router working correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
+
+// this is how we define paths in our app 
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+},
+{
+  path: 'about',
+  component: AboutComponent
+}
+];
 
 @NgModule({
   declarations: [
@@ -15,9 +27,13 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
